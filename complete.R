d00b707a94432756e350891d681b811e8501a3fb
Brian Rich <brian.d.rich@gmail.com>
2015-10-09 21:39:01 -0400

    commit after updated complete.R

--- complete.R
+++ complete.R
@@ -6,36 +6,25 @@ complete <- function(directory, id = 1:332) {
   nobs_vec <- numeric()
   
   for (i in id) {
+	
 	#read a specified CSV into file
     data <- read.csv(files[i])
     
     #create a data frame of only complete cases
     com_cases <- complete.cases(data)
     data <- data[com_cases, ] 
-    #data_s <- data[, "sulfate"]
-    #data_n <- data[, "nitrate"]
-    #data_all <- data.frame(data_s, data_n) 
       
-    #analyze frame to fine pairs of observations  
-    #analysis_frame <- data.frame(data_all, bothObs = (data_all$data_s > data_all$data_n))
-    
-    #count the number of results that have complete observations
-    #count_vec <- analysis_frame[, "bothObs"]
-    #count <- sum(length(which(!is.na(count_vec))))
+    #count the number of rows in the complete data frame
     count <- nrow(data)
     
     #add the current record ID and its count to the vectors
     id_vec <- append(id_vec, i)
     nobs_vec <- append(nobs_vec, count)
-    
-    #clean NAs from vectors
-    id_clean <- id_vec[which(!is.na(id_vec))]
-   	nobs_clean <- nobs_vec[which(!is.na(nobs_vec))]
   	
   	}
  
  	#create output data frame from clean vectors and name columns
-  	output <- data.frame(id_clean, nobs_clean)
+  	output <- data.frame(id_vec, nobs_vec)
   	colnames(output) <- c("id", "nobs")
   	output
 
